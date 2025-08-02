163bc1d118ac9c368f7dc37ee00f4960bb615268
TeacherSpaceSchool <azyk.store@gmail.com>
2025-08-02 22:43:01 +0600

    check

--- components/dialog/BuyBasket.js
+++ components/dialog/BuyBasket.js
@@ -26,7 +26,6 @@ import { getDeliveryDate } from '../../src/gql/deliveryDate';
 import { pdDDMMYYYYWW } from '../../src/lib';
 import { putOfflineOrders } from '../../src/service/idb/offlineOrders';
 
-/*test*/
 const BuyBasket =  React.memo(
     (props) =>{
         const {isMobileApp} = props.app;
