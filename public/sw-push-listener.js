5d8000e0287144aebb8b78e5b1b9412475ac22fe
TeacherSpaceSchool <azyk.store@gmail.com>
2025-12-05 18:08:31 +0600

    check

--- public/sw-push-listener.js
+++ public/sw-push-listener.js
@@ -17,29 +17,31 @@ self.addEventListener('push', function (event) {
         event.data.json()
         :
         {title: 'AZYK.STORE', message: 'Не забудьте сделать свой заказ', tag: 'AZYK.STORE', url: 'https://azyk.store', icon: 'https://azyk.store/static/192x192.png'};
-    event.waitUntil((async () => {
-        if(_data.type === 'forceUpdate') {
-            // Очистка всех кэшей
-            const cacheNames = await caches.keys();
-            // eslint-disable-next-line no-undef
-            await Promise.all(cacheNames.map(name => caches.delete(name)));
-
-            // Перезагрузка всех вкладок
-            const clientList = await self.clients.matchAll({ type: 'window' });
-            for (const client of clientList) {
-                client.navigate(client.url);
-            }
-        }
-        self.registration.showNotification(_data.title, {
-            badge: 'https://azyk.store/static/192x192.png',
-            body: _data.message,
-            icon: _data.icon,
-            tag: _data.tag,
-            silent: false,
-            vibrate: [200, 100, 200, 100, 200, 100, 200],
-            data: _data
-        })
-    })());
+    if (_data.type === 'forceUpdate') {
+        event.waitUntil(
+            caches.keys().then(cacheNames =>
+                // eslint-disable-next-line no-undef
+                Promise.all(cacheNames.map(cacheName => caches.delete(cacheName)))
+            ).then(() => {
+                // Перезагрузка всех открытых вкладок
+                self.clients.matchAll({ type: 'window' }).then(clientList => {
+                    clientList.forEach(client => client.navigate(client.url));
+                });
+            })
+        );
+    }
+    else
+        event.waitUntil(
+            self.registration.showNotification(_data.title, {
+                badge: 'https://azyk.store/static/192x192.png',
+                body: _data.message,
+                icon: _data.icon,
+                tag: _data.tag,
+                silent: false,
+                vibrate: [200, 100, 200, 100, 200, 100, 200],
+                data: _data
+            })
+        );
 });
 
 //notification url redirect event click
@@ -74,12 +76,4 @@ self.addEventListener('notificationclick', function (event) {
     );
 
 
-});
-
-// Принудительная перезагрузка вкладки при смене SW
-self.addEventListener('controllerchange', () => {
-    // eslint-disable-next-line no-undef
-    clients.matchAll({ type: 'window' }).then(clientList => {
-        clientList.forEach(client => client.navigate(client.url));
-    });
 });
